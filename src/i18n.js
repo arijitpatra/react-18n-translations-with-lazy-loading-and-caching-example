0fb994aff307c7363acf5a9988b2493c1e590e4e
Arijit Patra <ari.clicks@gmail.com>
2022-10-31 22:19:43 +0100

    cleanup
    cleanup

--- src/i18n.js
+++ src/i18n.js
@@ -4,49 +4,29 @@ import ChainedBackend from "i18next-chained-backend";
 import HttpBackend from "i18next-http-backend";
 import LocalStorageBackend from "i18next-localstorage-backend";
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-// const resources = {
-//   en: {
-//     translation: {
-//       "Welcome to React": "Welcome to React and react-i18next"
-//     }
-//   },
-//   fr: {
-//     translation: {
-//       "Welcome to React": "Bienvenue à React et react-i18next"
-//     }
-//   }
-// };
-
 i18n
   .use(initReactI18next)
   .use(ChainedBackend)
   .init({
-    // resources,
-    lng: 'hi_IN', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
+    lng: "hi_IN",
     interpolation: {
-      escapeValue: false, // react already safes from xss
+      escapeValue: false,
     },
     react: {
       useSuspense: true,
     },
     saveMissing: true,
     backend: {
-        backends: [
-          LocalStorageBackend,
-          HttpBackend
-        ],
-        backendOptions: [{
-          expirationTime: 7 * 24 * 60 * 60 * 1000 // 7 days
-        }, {
-          loadPath: '/locales/{{lng}}/{{ns}}.json'
-        }]
-      }
+      backends: [LocalStorageBackend, HttpBackend],
+      backendOptions: [
+        {
+          expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
+        },
+        {
+          loadPath: "/locales/{{lng}}/{{ns}}.json",
+        },
+      ],
+    },
   });
 
-  export default i18n;
+export default i18n;
